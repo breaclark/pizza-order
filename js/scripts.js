a120af5e731a40c028a681572cf354b1d4b44e1b
Brea Borlas <breatortilla@gmail.com>
2018-04-06 08:50:36 -0700

    Fulfill first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+function Pizza(size) {
+  this.size = size;
+  this.extras = [];
+  this.price = 5;
+}
+
+
+function makePizza(size, toppings){
+  pizza = new Pizza(size);
+  return pizza.price;
+}
+
+
+
+//front end logic
 $(document).ready(function() {
-  
+  $("#pizza-order").submit(function(event){
+    event.preventDefault();
+    console.log(makePizza("mini","nothing"));
+  });
 });
