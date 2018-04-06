9884d4899fa34a52a19a4c87200ecd220ba91e5b
Brea Borlas <breatortilla@gmail.com>
2018-04-06 10:59:15 -0700

    Position sauce and mozarella and add functions to change them

--- js/scripts.js
+++ js/scripts.js
@@ -34,12 +34,28 @@ function makePizza(baseValue, toppingsArray){
 function pizzaSizeAdjuster(sizeValue){
   if (sizeValue === "Small") {
     $("#pizza-img").css("width","50%");
+    $("#sauce-img").css("width","40%");
+    $("#sauce-img").css("margin-top","4%");
+    $("#cheese-img").css("width","38%");
+    $("#cheese-img").css("margin-top","5%");
   } else if (sizeValue === "Medium") {
     $("#pizza-img").css("width","70%");
+    $("#sauce-img").css("width","60%");
+    $("#sauce-img").css("margin-top","4%");
+    $("#cheese-img").css("width","55%");
+    $("#cheese-img").css("margin-top","6%");
   } else if (sizeValue === "Large") {
     $("#pizza-img").css("width","90%");
+    $("#sauce-img").css("width","75%");
+    $("#sauce-img").css("margin-top","7%");
+    $("#cheese-img").css("width","70%");
+    $("#cheese-img").css("margin-top","9%");
   } else {
     $("#pizza-img").css("width","30%");
+    $("#sauce-img").css("width","25%");
+    $("#sauce-img").css("margin-top","2%");
+    $("#cheese-img").css("width","23%");
+    $("#cheese-img").css("margin-top","3%");
   }
 }
 
@@ -50,6 +66,22 @@ function pizzaCrustAdjuster(crustValue){
     $("#pizza-img").attr("src", "img/stuffed.png")
   } else {
     $("#pizza-img").attr("src", "img/pan.png")
+  }
+}
+
+function pizzaSauceAdjuster(sauceValue){
+  if (sauceValue === "Barbecue") {
+    $("#sauce-img").attr("src", "img/barbecue.png")
+  } else {
+    $("#sauce-img").attr("src", "img/marinara.png")
+  }
+}
+
+function pizzaCheeseAdjuster(cheeseValue){
+  if (cheeseValue === "Mozarella") {
+    $("#cheese-img").attr("src", "img/mozarella.png")
+  } else {
+    $("#cheese-img").attr("src", "img/house.png")
   }
 }
 
@@ -67,6 +99,8 @@ $(document).ready(function() {
   });
   $(".form-control").change(function(){
     pizzaSizeAdjuster($("#size option:selected").text());
-    pizzaCrustAdjuster($("#crust option:selected").text())
+    pizzaCrustAdjuster($("#crust option:selected").text());
+    pizzaSauceAdjuster($("#sauce option:selected").text());
+    pizzaCheeseAdjuster($("#cheese option:selected").text());
   });
 });
